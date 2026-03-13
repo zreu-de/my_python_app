845c5cad1bd2c54fe495e2ba8bdacfa8b17ed51b
zreu-de <z.055reu@gmail.com>
2026-03-13 10:20:24 +0200

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,17 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from app import add
+
 def test_add_positive():
     assert add(2, 3) == 5
+
 def test_add_zero():
     assert add(5, 0) == 5
+
 def test_add_negative():
     assert add(-2, -3) == -5
+
 def test_add_mixed():
     assert add(-2, 3) == 1
